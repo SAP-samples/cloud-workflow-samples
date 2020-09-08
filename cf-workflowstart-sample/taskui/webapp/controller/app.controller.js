dbe7d154a0ccb30ef76d4ad942a6efb9c2b3a85d
Archana Shukla <34297037+ArchanaShukla@users.noreply.github.com>
2020-09-08 21:39:10 +0530

    Modified for external service call

--- cf-workflowstart-sample/taskui/webapp/controller/app.controller.js
+++ cf-workflowstart-sample/taskui/webapp/controller/app.controller.js
@@ -1,15 +1,28 @@
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	],
+    "sap/ui/core/mvc/Controller"
+],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-	function (Controller) {
-		"use strict";
+    function (Controller) {
+        "use strict";
 
-		return Controller.extend("sap.demo.bpm.taskui.controller.app", {
-			onInit: function () {
+        return Controller.extend("sap.demo.bpm.taskui.controller.app", {
+            onInit: function () {
+                var that = this;
+                $.ajax({
+                    url: "/sapdemobpmtaskui/NorthWind_Dest/V3/Northwind/Northwind.svc/Suppliers?$format=json",
+                    method: "GET",
+                    async: false,
+                    success: function (result, xhr, data) {
+                        var supplierData = data.responseJSON.value;
+                        var existingData = that.getView().getModel();
+                        existingData.oData.Suppliers = {};
+                        existingData.oData.Suppliers = supplierData;
+                        that.getView().setModel(existingData);
+                    }
+                });
 
-			}
-		});
-	});
+            }
+        });
+    });
