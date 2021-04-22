f0cddbb2ee51da328756c75cdf73844e2e563e23
Archana Shukla <34297037+ArchanaShukla@users.noreply.github.com>
2021-04-22 13:59:03 +0530

    Deleting the unwanted files

--- cf-aif-workflow-integration-sample/AIFAlertManagement/AIFAlertUI/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"com/sap/aif/AIFAlertUI/test/unit/controller/Root.controller"
-], function () {
-	"use strict";
-});
--- cf-aif-workflow-integration-sample/AIFAlertManagement/AIFAlertWorkflow/scripts/AIFAlert/ModifyContext.js
+++
@@ -1,5 +0,0 @@
-//$.context.taskProcessor = $.context.initiator;
-var ibn = $.context.alertData.ibn;
-ibn = ibn.replace("%252f", "%2f"); // Fix the encode error (if there is)
-$.context.alertData.ibn = ibn;
-
